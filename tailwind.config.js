140f8e7fd6e618d5d53142972df4e76b8e6ce9e9
Hagen <hagen.hoferichter@gmail.com>
2022-06-24 22:23:41 +0200

    new layout

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  content: ['./src/**/*.{svelte,js,ts}'],
+//   daisyui: {
+//     themes: [
+//       {
+//         mytheme: {
+//
+// "primary": "#e28897",
+//
+// "secondary": "#d2dd68",
+//
+// "accent": "#d6196e",
+//
+// "neutral": "#2B2C36",
+//
+// "base-100": "#381F47",
+//
+// "info": "#579CEF",
+//
+// "success": "#38D6C6",
+//
+// "warning": "#ECB746",
+//
+// "error": "#E85E88",
+//         },
+//       },
+//     ],
+//   },
+  plugins: [require('daisyui')],
+};
